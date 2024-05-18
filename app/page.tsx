f7417486b98ae91b72ad5eaf6ea29510b35df281
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-18 14:59:13 +0530

    define structure of grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { FaHome } from "react-icons/fa";
 import Image from "next/image";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 bg-black-100">
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
